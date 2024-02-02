adef0990e37435ec6a8f6583c58650ce8d2ba599
Nikita Tishkov <nikitatishkou@gmail.com>
2024-02-02 17:16:43 +0300

    update cors policy

--- src/services/Identity/Identity.API/Configurations/CorsPolicyConfiguration.cs
+++ src/services/Identity/Identity.API/Configurations/CorsPolicyConfiguration.cs
@@ -2,14 +2,17 @@
 {
     public static class CorsPolicyConfiguration
     {
-        public static void AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration) =>
-           services.AddCors(options =>
-           {
-               options.AddPolicy("CorsPolicy", builder =>
-                  builder.AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithOrigins(configuration["CorsPolicy:AllowedOrigins"]!));
-           });
+        public static void AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder => builder.AllowAnyMethod()
+                                 .AllowAnyHeader()
+                                 .AllowCredentials()
+                                 .WithOrigins(configuration["CorsPolicy:AllowedOrigins"]!
+                ));
+            });
+        }
     }
 }
