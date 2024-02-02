9e0769537818800bd3757fe3ffdbff6fa1c772d5
Nikita Tishkov <nikitatishkou@gmail.com>
2024-02-02 17:18:11 +0300

    update dtos

--- src/services/Identity/Identity.Services/Dtos/RegistrationUserDto.cs
+++ src/services/Identity/Identity.Services/Dtos/RegistrationUserDto.cs
@@ -3,6 +3,7 @@
     public class RegistrationUserDto
     {
         public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
--- src/services/Identity/Identity.Services/Dtos/RequestDtos/RequestUserDto.cs
+++ src/services/Identity/Identity.Services/Dtos/RequestDtos/RequestUserDto.cs
@@ -5,5 +5,6 @@
         public string Email { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
     }
 }
