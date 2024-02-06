398b355bfa3969e9e57e04acb75d71aae9a2848d
Nikita Tishkov <nikitatishkou@gmail.com>
2024-02-06 10:25:03 +0300

    add serilog

--- src/services/Identity/Identity.API/Configurations/SerilogElasticsearchConfiguration.cs
+++ src/services/Identity/Identity.API/Configurations/SerilogElasticsearchConfiguration.cs
@@ -6,11 +6,11 @@ namespace Identity.API.Configurations
 {
     public static class SerilogElasticsearchConfiguration
     {
-        public static void AddLoggerConfiguration(this IServiceCollection services, IConfiguration configuration)
+        public static void AddLoggerConfiguration(this IServiceCollection services, WebApplicationBuilder builder)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-            var elkconfiguration = new ConfigurationBuilder()
+            var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: true)
                 .Build();
@@ -18,12 +18,12 @@ namespace Identity.API.Configurations
             Log.Logger = new LoggerConfiguration()
                 .Enrich.WithExceptionDetails()
                 .WriteTo.Console()
-                .WriteTo.Elasticsearch(ConfigureElasticsearchSink(elkconfiguration))
+                .WriteTo.Elasticsearch(ConfigureElasticsearchSink(configuration))
                 .Enrich.WithProperty("Environment", environment)
-                .ReadFrom.Configuration(configuration)
+                .ReadFrom.Configuration(builder.Configuration)
                 .CreateLogger();
 
-            //builder.Host.UseSerilog();
+            builder.Host.UseSerilog();
         }
 
         private static ElasticsearchSinkOptions ConfigureElasticsearchSink(IConfigurationRoot configuration)
