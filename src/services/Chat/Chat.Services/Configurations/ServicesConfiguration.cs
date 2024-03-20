8696323539e4b4c9d64dc89c96d485ecd4c2bd2c
Nikita Tishkov <nikitatishkou@gmail.com>
2024-03-20 17:28:11 +0300

    add di conf

---
+++ src/services/Chat/Chat.Services/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat.Services.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddServicesConfiguration(this IServiceCollection services)
+        {
+
+        }
+    }
+}
