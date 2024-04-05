4615c4785e6982a5a9a78470d0ad35cc4004d72b
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-05 15:40:01 +0300

    recover lose record

--- src/services/Hunting/Modules/Animal/Modules.Animal.Application/Features/Animal/Queries/GetAnimalById.cs
+++ src/services/Hunting/Modules/Animal/Modules.Animal.Application/Features/Animal/Queries/GetAnimalById.cs
@@ -3,5 +3,5 @@ using Modules.Animal.Application.Dtos.ResponseDtos;
 
 namespace Modules.Animal.Application.Features.Animal.Queries
 {
-    public class GetAnimalById(Guid Id) : IRequest<AnimalInfoResponseDto>;
+    public record class GetAnimalById(Guid Id) : IRequest<AnimalInfoResponseDto>;
 }
