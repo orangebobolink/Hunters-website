0e065fdc4aae44df3be6748fa1f2bda3e7f530f5
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-05 15:01:45 +0300

    add folders for mediatr

---
+++ src/services/Hunting/Modules/Animal/Modules.Animal.Core/Commands/AnimalCreateCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Modules.Animal.Domain.Entities;
+using Modules.Animal.Domain.Enums;
+
+namespace Modules.Animal.Domain.Commands
+{
+    public class AnimalCreateCommand : IRequest<AnimalInfo>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public AnimalType Type { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
