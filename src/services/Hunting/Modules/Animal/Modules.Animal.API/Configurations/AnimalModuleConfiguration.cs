9ea3813ea3f9c0361f2504f3841fff38db72bb3a
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-11 14:07:55 +0300

    add validators

--- src/services/Hunting/Modules/Animal/Modules.Animal.API/Configurations/AnimalModuleConfiguration.cs
+++ src/services/Hunting/Modules/Animal/Modules.Animal.API/Configurations/AnimalModuleConfiguration.cs
@@ -11,6 +11,7 @@ namespace Modules.Animal.API.Configurations
         {
             services.AddMediatRConfiguration();
             services.AddInfrastructureConfiguration(configuration);
+            services.AddFluentValidationConfiguration();
         }
     }
 }
---
+++ src/services/Hunting/Modules/Animal/Modules.Animal.API/Validators/HuntingSeasonRequestDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Modules.Animal.Application.Dtos.RequestDtos;
+
+namespace Modules.Animal.API.Validators
+{
+    public class HuntingSeasonRequestDtoValidator
+         : AbstractValidator<HuntingSeasonRequestDto>
+    {
+        public HuntingSeasonRequestDtoValidator()
+        {
+            RuleFor(season => season.StartDate)
+                .NotEmpty()
+                .WithMessage("Start date is required.")
+                .LessThan(season => season.EndDate)
+                .WithMessage("Start date must be before the end date.");
+
+            RuleFor(season => season.EndDate)
+                .NotEmpty()
+                .WithMessage("End date is required.")
+                .GreaterThan(season => season.StartDate)
+                .WithMessage("End date must be after the start date.");
+
+            RuleFor(season => season.WayOfHunting)
+                .NotEmpty()
+                .WithMessage("Way of hunting is required.")
+                .MaximumLength(100)
+                .WithMessage("Way of hunting must not exceed 100 characters.");
+        }
+    }
+}
