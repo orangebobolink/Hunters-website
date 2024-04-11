b7780a8c729de7407c1e888e385fa83b447b7a64
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-11 15:37:49 +0300

    add new method to repository

--- src/services/Hunting/Modules/Animal/Modules.Animal.Core/Interfaces/Repositories/IHuntingSeasonRepository.cs
+++ src/services/Hunting/Modules/Animal/Modules.Animal.Core/Interfaces/Repositories/IHuntingSeasonRepository.cs
@@ -5,7 +5,14 @@ namespace Modules.Animal.Domain.Interfaces.Repositories
     public interface IHuntingSeasonRepository 
         : IRepository<HuntingSeason>
     {
-        public Task<List<HuntingSeason>> GetHuntingSeasonsByTimePeriod(DateTime startDate, DateTime endDate, 
-                                                                        CancellationToken cancellationToken);
+        public Task<List<HuntingSeason>> GetHuntingSeasonsByTimePeriod(
+            DateTime startDate, 
+            DateTime endDate, 
+            CancellationToken cancellationToken);
+        public Task<HuntingSeason?> GetHuntingSeasonByTimePeriodForAnimal(
+            Guid animalId,
+            DateTime startDate, 
+            DateTime endDate,
+            CancellationToken cancellationToken);
     }
 }
--- src/services/Hunting/Modules/Animal/Modules.Animal.Infrastructure/Repositories/HuntingSeasonRepository.cs
+++ src/services/Hunting/Modules/Animal/Modules.Animal.Infrastructure/Repositories/HuntingSeasonRepository.cs
@@ -8,29 +8,47 @@ namespace Modules.Animal.Infrastructure.Repositories
     internal class HuntingSeasonRepositpry(ApplicationDbContext context)
         : BaseRepository<HuntingSeason>(context), IHuntingSeasonRepository
     {
-        public override async Task<List<HuntingSeason>> GetAllAsync(CancellationToken cancellationToken)
+        public override async Task<List<HuntingSeason>> GetAllAsync(
+            CancellationToken cancellationToken)
         {
             return await _context.HuntingSeasons
                 .AsNoTracking()
                 .ToListAsync(cancellationToken);
         }
 
-        public override async Task<HuntingSeason?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        public override async Task<HuntingSeason?> GetByIdAsync(
+            Guid id, 
+            CancellationToken cancellationToken)
         {
             return await _context.HuntingSeasons
                 .AsNoTracking()
                 .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
         }
 
-        public async Task<List<HuntingSeason>> GetHuntingSeasonsByTimePeriod(DateTime startDate, 
-                                                                                DateTime endDate, 
-                                                                                CancellationToken cancellationToken)
+        public async Task<List<HuntingSeason>> GetHuntingSeasonsByTimePeriod(
+            DateTime startDate, 
+            DateTime endDate, 
+            CancellationToken cancellationToken)
         {
             return await _context.HuntingSeasons
                 .AsNoTracking()
                 .Where(hs => hs.StartDate >= startDate 
                             && hs.EndDate <= endDate)
                 .ToListAsync(cancellationToken);
+        }
+
+        public async Task<HuntingSeason?> GetHuntingSeasonByTimePeriodForAnimal(
+            Guid animalId, 
+            DateTime startDate, 
+            DateTime endDate,
+            CancellationToken cancellationToken)
+        {
+            return await _context.HuntingSeasons
+                .AsNoTracking()
+                .FirstOrDefaultAsync(hs => hs.AnimalId == animalId
+                                      && hs.StartDate >= startDate 
+                                      && hs.EndDate <= endDate, 
+                    cancellationToken);
         }
     }
 }
